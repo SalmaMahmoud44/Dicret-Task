2b660bb701da4b7246df2dfd12c688a1961c68e9
SalmaMahmoud44 <121463290+SalmaMahmoud44@users.noreply.github.com>
2022-12-27 00:40:30 +0200

    Add files via upload

---
+++ project 1/project 1/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+
+            Console.WriteLine("Enter the lower limit: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the upper limit: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The prime numbers between {0} and {1} are: ", n1, n2);
+
+            for (int i = n1; i <= n2; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            int boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
+
+
---
+++ project 2/project 2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace PerfectNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+
+            Console.WriteLine("Enter the lower limit: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the upper limit: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The perfect numbers between {0} and {1} are: ", n1, n2);
+
+            for (int i = n1; i <= n2; i++)
+            {
+                if (IsPerfect(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static bool IsPerfect(int number)
+        {
+            int sum = 0;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum == number;
+        }
+    }
+}
